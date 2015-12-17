e7a62085a00ebb9134a13a96263ade63581824b8
quadstar <icberg27@gmail.com>
2015-12-16 19:46:30 -0600

    created mongo schemas and databases

--- server/server.js
+++ server/server.js
@@ -3,6 +3,12 @@ var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 var path = require('path');
+var mongo = require('./db.js');
+var db = mongo.db;
+var models = require('./models.js');
+var Quest = models.Quest;
+var Step = models.Step;
+
 app.use(bodyParser.json());
 
 app.set('port', 3000);
@@ -11,6 +17,20 @@ app.get('/', function(req, res){
     res.sendFile(path.join(__dirname + '/../client/index.html'));
 });
 
+app.post('/testing', function(req, res){
+	Quest.find(req.body).then(function(quests){
+		// res.send(req.body);
+		var newQuest = new Quest(req.body);
+		newQuest.save(function(err, result){
+			if(err){
+				console.log('help');
+				throw err;
+			}
+			res.send(result);
+		});
+		
+	});
+})
 
 // Wildcare Files
 app.get('/*', function(req, res){
