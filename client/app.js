ec10d1b3e5886325e540915d90bbdf0747d85f62
thecitywelivein <thecitywelivein@gmail.com>
2015-12-17 12:09:29 -0600

    Built QuestView Page

--- client/app.js
+++ client/app.js
@@ -9,10 +9,11 @@ app.config(function(){});
 
 app.run(function($rootScope){});
 
-
-app.controller('MyController', function ($scope, $location, $window) {
+angular.module('cityQuestApp', []).controller('MyController', function ($scope, $window, $location) {
+  $scope.test = "Hello";
   $scope.city = "";
-  $scope.quests = [{
+  $scope.quests = [
+  {
     "id": 0,
     "name": "Down in the park",
     "description": "The most perfect summer day",
@@ -21,32 +22,66 @@ app.controller('MyController', function ($scope, $location, $window) {
     "time": 120,
     "cost": 7,
     "location": "30.267219, -97.764799",
+    "image":"http://www.edwardsaquifer.net/images/barton_main_spring.jpg",
     "steps" : [  
-
       {
-        "quest_id":0,
         "number":0,
         "description":"Go take a dip at Barton Springs",
         "location": "30.264439, -97.771279",
-        "time": "30",
-        "cost": "4",
+        "time": 30,
+        "cost": 4,
       },
       {
-        "quest_id":0,
         "number":1,
         "description":"Drink Watermelon Juice",
         "location": "30.264439, -97.771279",
-        "time": "30",
-        "cost": "3",
+        "time": 30,
+        "cost": 3,
       },
       {
-        "quest_id":0,
         "number":2,
         "description":"Play Frisbee",
         "location": "30.267812, -97.774069",
-        "time": "60",
-        "cost": "0",
+        "time": 60,
+        "cost": 0,
       }
+
+    ]
+
+  },
+  {
+    "id": 1,
+    "name": "Down the River",
+    "description": "lady bird lake y'all",
+    "tags" : "Cheap, Nature, Dogs, Park",
+    "city": "Austin, TX",
+    "time": 120,
+    "cost": 3,
+    "location": "30.267219, -97.764799",
+    "image":"http://thingstodo.viator.com/austin/files/2013/07/4791083511_49703bb679_z.jpg",
+    "steps" : [  
+      {
+        "number":0,
+        "description":"Go take a dip at Barton Springs",
+        "location": "30.264439, -97.771279",
+        "time": 30,
+        "cost": 4,
+      },
+      {
+        "number":1,
+        "description":"Drink Watermelon Juice",
+        "location": "30.264439, -97.771279",
+        "time": 30,
+        "cost": 3,
+      },
+      {
+        "number":2,
+        "description":"Play Frisbee",
+        "location": "30.267812, -97.774069",
+        "time": 60,
+        "cost": 0,
+      }
+
     ]
 
   }];
