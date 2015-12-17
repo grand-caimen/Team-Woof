95f54bb5869271cac9d1048eb0cbcb98e7f59e59
John D <redline@Johns-MacBook-Air.local>
2015-12-17 10:16:30 -0600

    Added

--- client/app.js
+++ client/app.js
@@ -1,10 +1,39 @@
-var app = angular.module('cityQuestApp',[
+var cityQuestApp = angular.module('cityQuestApp',[
                          'cityQuest.services',
                          'cityQuest.city',
                          'cityQuest.questList',
                          'cityQuest.questView',
                          'cityQuest.createQuest']);
 
-app.config(function(){});
+cityQuestApp.config(['$routeProvider',
+                    routeDefinition]);
 
-app.run(function($rootScope){});
+cityQuestApp.run(function($rootScope){});
+
+function routeDefinition($routeProvider){
+  $routeProvider
+  .when('/', {
+    templateUrl: 'city/city.html',
+    controller: 'cityCtrl'
+  })
+  .when('/city', {
+    templateUrl: 'city/city.html',
+    controller: 'cityCtrl'
+  })
+  .when('/questList', {
+    templateUrl: 'questList/questList.html',
+    controller: 'questListCtrl'
+  })
+  .when('/questView', {
+    templateUrl: 'questView/questView.html',
+    controller: 'questViewCtrl'
+  })
+  .when('/createView', {
+    templateUrl: 'createView/createView.html',
+    controller: 'createViewCtrl'
+  })
+  .otherwise({
+    redirectTo: '/'
+  });
+}
+
