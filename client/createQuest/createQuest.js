7534651ce9783d3f8f23f6195bc7aefa495a5fcf
Ryan Hamblin <ryanhca@gmail.com>
2015-12-17 17:15:41 -0600

    created post controller

--- client/createQuest/createQuest.js
+++ client/createQuest/createQuest.js
@@ -1,5 +1,14 @@
 angular.module('cityQuest.createQuest', [])
 
-.controller('createQuestCtrl', function($scope, QuestStorage){
+.controller('createQuestCtrl', function($scope, $location, QuestStorage){
+
+	 $scope.quest = {};
+
+	 $scope.questCreate = function(){
+			QuestStorage.saveNewQuest($scope.quest);
+			$location.path('/questList');
+	 };
+
+
 
 });
