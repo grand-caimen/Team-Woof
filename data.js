b2be7fba714cce5c60b27d591b7cec5058d8c524
Kayla Matteo <kaylamatteo@gmail.com>
2015-12-17 19:59:43 -0600

    data

---
+++ data.js
@@ -0,0 +1,201 @@
+{
+   "id": 0,
+   "name": "Down in the park",
+   "description": "The most perfect summer day",
+   "tags" : "Cheap, Nature, Dogs, Park",
+   "city": "Austin",
+   "time": 120,
+   "cost": 7,
+   "location": "30.267219, -97.764799",
+   "image":"http://www.edwardsaquifer.net/images/barton_main_spring.jpg (166KB)
+
+",
+   "steps" : [  
+     {
+       "number":0,
+       "description":"Go take a dip at Barton Springs",
+       "location": "30.264439, -97.771279",
+       "time": 30,
+       "cost": 4,
+     },
+     {
+       "number":1,
+       "description":"Drink Watermelon Juice",
+       "location": "30.264439, -97.771279",
+       "time": 30,
+       "cost": 3,
+     },
+     {
+       "number":2,
+       "description":"Play Frisbee",
+       "location": "30.267812, -97.774069",
+       "time": 60,
+       "cost": 0,
+     }
+
+   ]
+
+ },
+ {
+   "id": 1,
+   "name": "Down the River",
+   "description": "lady bird lake y'all",
+   "tags" : "Cheap, Nature, Dogs, Park",
+   "city": "Austin",
+   "time": 120,
+   "cost": 3,
+   "location": "30.267219, -97.764799",
+   "image":"http://thingstodo.viator.com/austin/files/2013/07/4791083511_49703bb679_z.jpg (190KB)
+
+",
+   "steps" : [  
+     {
+       "number":0,
+       "description":"Go take a dip at Barton Springs",
+       "location": "30.264439, -97.771279",
+       "time": 30,
+       "cost": 4,
+     },
+     {
+       "number":1,
+       "description":"Drink Watermelon Juice",
+       "location": "30.264439, -97.771279",
+       "time": 30,
+       "cost": 3,
+     },
+     {
+       "number":2,
+       "description":"Play Frisbee",
+       "location": "30.267812, -97.774069",
+       "time": 60,
+       "cost": 0,
+     }
+
+   ],
+
+},
+  {
+    "id": 2,
+    "name": "Upper Manhattan Morning",
+    "description": "A morning stroll for history lovers",
+    "tags": "Nature, Park, Museum, Food",
+    "city": "New York",
+    "time": 210,
+    "cost": 30,
+    "location": "40.778023, -73.969303",
+    "image": "http://graphics8.nytimes.com/images/2012/11/15/arts/15artsbeat-museum/15artsbeat-museum-blog480.jpg",
+
+    "steps": [
+      { 
+        "number":0,
+        "description":"Grab a bagel with lox and coffee at historic Zabar's.",
+        "location": "40.784978, -73.979679",
+        "time": 30,
+        "cost": 5,
+      }
+      {
+        "number":1,
+        "description":"Take a leisurely stroll through Central Park.",
+        "location": "40.778023, -73.969303",
+        "time": 60,
+        "cost": 0,
+      }
+      {
+        "number":2,
+        "description":"Spend a few hours browsing the Metropolitan Mueseum of Art.",
+        "location": "40.779623, -73.963255",
+        "time": 120,
+        "cost": 25,
+      }
+      ]
+  },
+   {
+   "id": 3,
+   "name": "A Sunny Day in West Los Angeles",
+   "description": "Beach, shopping, and amusement in eternally sunny LA",
+   "tags" : "Nature, Shopping, Beach, Fun, Food",
+   "city": "Los Angeles",
+   "time": 660,
+   "cost": 70,
+   "location": "34.013250, -118.501021",
+   "image":"http://www.westland.net/piercam/images/santa-monica-pier-cam.jpg",
+
+   "steps" : [  
+     {
+       "number":0,
+       "description":"Start with a hearty diner breakfast at Cora's Coffee Shop.",
+       "location": "34.008402, -118.491312",
+       "time": 60,
+       "cost": 15,
+     },
+     {
+       "number":1,
+       "description":"Window shop and grab a picnic lunch on the Third Street Promenade.",
+       "location": "34.016466, -118.496891",
+       "time": 120,
+       "cost": 10,
+     },
+     {
+       "number":2,
+       "description":"Spend the afternoon at Santa Monica Beach.",
+       "location": "34.015881, -118.504753",
+       "time": 300,
+       "cost": 0,
+     },
+     {
+        "number":3,
+       "description":"Ride the ferris wheel on Santa Monica Pier.",
+       "location": "34.010114, -118.496379",
+       "time": 60,
+       "cost": 5,
+     },
+      {
+      "number":4,
+       "description":"Grab dinner and drinks at the swanky Viceroy Hotel.",
+       "location": "34.008277, -118.490306",
+       "time": 120,
+       "cost": 40,
+     },
+
+   ],
+
+},
+
+   {
+   "id": 3,
+   "name": "An Afternoon in Salt Lake City",
+   "description": "Pizza, Religion, and The Great Salt Lake.",
+   "tags" : "Architechture, Lake, Nature, Religion, Food",
+   "city": "Salt Lake City",
+   "time": 300,
+   "cost": 16,
+   "location": "40.730161, -111.897756",
+   "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDEDEKZs7o_1lv1uqI9idscw2ACNAfhgKQGVPtr80xiZCixs_wiw",
+
+   "steps" : [  
+     {
+       "number":0,
+       "description":"Snap some photos of historic Temple Square.",
+       "location": "40.773891, -111.890925",
+       "time": 30,
+       "cost": 0,
+     },
+     {
+       "number":1,
+       "description":"Eat some of the best pizza around at The Pie.",
+       "location": "40.626374, -111.858342",
+       "time": 90,
+       "cost": 10,
+     },
+     {
+       "number":2,
+       "description":"Take in the Great Lake at Antelope Island State Park.",
+       "location": "40.972657, -112.216952",
+       "time": 380,
+       "cost": 6,
+     },
+   ],
+
+},
+
+ }
