d9de6182bf034d2cb8832389f5d593e7df78dedf
thecitywelivein <thecitywelivein@gmail.com>
2015-12-19 16:42:40 -0600

    Completed Add Step Button

--- client/questList/questList.js
+++ client/questList/questList.js
@@ -2,88 +2,22 @@ angular.module('cityQuest.questList', [])
 
 .controller('questListCtrl', function($scope, QuestStorage){
   //Populate this object with variables 
-  // $scope.data = {};
   $scope.city = "";
+  $scope.showErr = false;
 
   $scope.quests = null;
-
-  // $scope.quests = [
-  // {
-  //   "id": 0,
-  //   "name": "Down in the park",
-  //   "description": "The most perfect summer day",
-  //   "tags" : [{ "text":"cheap"},{ "text":"nature"},{ "text":"dogs"}],
-  //   "city": "Austin, TX",
-  //   "time": 120,
-  //   "cost": 7,
-  //   "location": "30.267219, -97.764799",
-  //   "image":"http://www.edwardsaquifer.net/images/barton_main_spring.jpg",
-  //   "steps" : [  
-  //     {
-  //       "number":0,
-  //       "description":"Go take a dip at Barton Springs",
-  //       "location": "30.264439, -97.771279",
-  //       "time": 30,
-  //       "cost": 4,
-  //     },
-  //     {
-  //       "number":1,
-  //       "description":"Drink Watermelon Juice",
-  //       "location": "30.264439, -97.771279",
-  //       "time": 30,
-  //       "cost": 3,
-  //     },
-  //     {
-  //       "number":2,
-  //       "description":"Play Frisbee",
-  //       "location": "30.267812, -97.774069",
-  //       "time": 60,
-  //       "cost": 0,
-  //     }
-
-  //   ]
-
-  // },
-  // {
-  //   "id": 1,
-  //   "name": "Down the River",
-  //   "description": "lady bird lake y'all",
-  //   "tags" : [{ "text":"cheap"},{ "text":"nature"},{ "text":"dogs"}],
-  //   "city": "Austin, TX",
-  //   "time": 120,
-  //   "cost": 3,
-  //   "location": "30.267219, -97.764799",
-  //   "image":"http://thingstodo.viator.com/austin/files/2013/07/4791083511_49703bb679_z.jpg",
-  //   "steps" : [  
-  //     {
-  //       "number":0,
-  //       "description":"Go take a dip at Barton Springs",
-  //       "location": "30.264439, -97.771279",
-  //       "time": 30,
-  //       "cost": 4,
-  //     },
-  //     {
-  //       "number":1,
-  //       "description":"Drink Watermelon Juice",
-  //       "location": "30.264439, -97.771279",
-  //       "time": 30,
-  //       "cost": 3,
-  //     },
-  //     {
-  //       "number":2,
-  //       "description":"Play Frisbee",
-  //       "location": "30.267812, -97.774069",
-  //       "time": 60,
-  //       "cost": 0,
-  //     }
-
-  //   ]
-
-  // }];
 
   $scope.getAllQuests = function(){
     QuestStorage.getAllQuests()
     .then(function(quests){
+      if(quests.length>0){
+        quests.forEach(function(quest){
+          quest.time = minutesToHours(quest.time);
+          console.log(quest);
+        })
+      }else{
+        $scope.showErr = true;
+      }
       $scope.quests = quests;
     })
     .catch(function(error){
@@ -92,13 +26,5 @@ angular.module('cityQuest.questList', [])
   }
 
   $scope.getAllQuests();
-
-  //TODO - uncomment to stop using fake data - John
-  //$scope.getAllQuests();
-  //Remove the following function plus corresponding in services.js
-  // function initializeQuestStorageWithFakeData(quests){
-  //   QuestStorage.initializeWithFakeData(quests);
-  // }
-  // initializeQuestStorageWithFakeData($scope.quests);
 
 });
